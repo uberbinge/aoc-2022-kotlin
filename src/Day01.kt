19d556a0225fa68597e9c99e83eaf25487a64acd
Waqas Ahmed <mail@waqas.dev>
2022-12-01 23:09:10 +0100

    get rid of extra max calculationˆ

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,20 +1,16 @@
 fun main() {
     val input = readInput("Day01")
-    var max = Integer.MIN_VALUE
     var sum = 0
-    val elvesSum  = mutableListOf<Int>()
+    val elvesSum = mutableListOf<Int>()
     input.forEach {
         if (it.toIntOrNull() != null) {
             sum += it.toInt()
-            if (sum > max) {
-                max = sum
-            }
         } else {
             elvesSum.add(sum)
             sum = 0
         }
     }
     elvesSum.sortDescending()
-    println(elvesSum[0])
-    elvesSum.take(3).sum().also { println(it) }
+    assert(69883 == elvesSum[0])
+    assert(207576 == elvesSum.take(3).sum())
 }
