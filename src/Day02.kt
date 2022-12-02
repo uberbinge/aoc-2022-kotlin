1cabfdbb5ead4f8654e7f7d984c4becb651e5bcf
Waqas Ahmed <mail@waqas.dev>
2022-12-02 22:49:38 +0100

    nits

--- src/Day02.kt
+++ src/Day02.kt
@@ -1,27 +1,23 @@
+fun main() {
+    firstProblem()
+    secondProblem()
+}
+
 fun firstProblem() {
 
     val input = readInput("Day02")
     var sum = 0
-    input.forEach {
-        val a = it.split(" ")[0]
-        val b = it.split(" ")[1]
-        val first = convertToRockPaperScissors(a)
-        val second = convertToRockPaperScissors(b)
+    val newInput = input.asSequence().map { it.replace("A", "rock") }.map { it.replace("B", "paper") }.map { it ->
+        it.replace("C", "scissors")
+    }.map { it.replace("X", "rock") }.map { it.replace("Y", "paper") }.map {
+        it.replace("Z", "scissors")
+    }.toList()
+
+    newInput.forEach {
+        val first = it.split(" ")[0]
+        val second = it.split(" ")[1]
         // check winLoseOrDraw
-        val result = checkWinLoseOrDraw(second, first)
-        sum += when (result) {
-            Result.Win -> {
-                6
-            }
-
-            Result.Lose -> {
-                0
-            }
-
-            else -> {
-                3
-            }
-        }
+        sum += checkWinLoseOrDraw(second, first)
         // check if second is rock, paper or scissors and add to sum 1,2 or 3 respectively
         sum += when (second) {
             "rock" -> {
@@ -42,19 +38,17 @@ fun firstProblem() {
     check(13924 == sum)
 }
 
-fun main() {
-    firstProblem()
-    secondProblem()
-}
-
 fun secondProblem() {
     val input = readInput("Day02")
     var sum = 0
-    input.forEach {
-        val a = it.split(" ")[0]
-        val b = it.split(" ")[1]
-        val first = convertToRockPaperScissors(a)
-        val convertedMove = when (convertToRockPaperScissors(b)) {
+    val newInput = input.asSequence().map { it.replace("A", "rock") }.map { it.replace("B", "paper") }.map { it ->
+        it.replace("C", "scissors")
+    }.map { it.replace("X", "rock") }.map { it.replace("Y", "paper") }.map {
+        it.replace("Z", "scissors")
+    }.toList()
+    newInput.forEach {
+        val first = it.split(" ")[0]
+        val convertedMove = when (it.split(" ")[1]) {
             "rock" -> {
                 convertToLosingMove(first)
             }
@@ -68,20 +62,7 @@ fun secondProblem() {
             }
         }
         // check winLoseOrDraw
-        val result = checkWinLoseOrDraw(convertedMove, first)
-        sum += when (result) {
-            Result.Win -> {
-                6
-            }
-
-            Result.Lose -> {
-                0
-            }
-
-            else -> {
-                3
-            }
-        }
+        sum += checkWinLoseOrDraw(convertedMove, first)
         // check if second is rock, paper or scissors and add to sum 1,2 or 3 respectively
         sum += when (convertedMove) {
             "rock" -> {
@@ -102,40 +83,19 @@ fun secondProblem() {
     check(13448 == sum)
 }
 
-fun checkWinLoseOrDraw(player1: String, player2: String): Result {
+fun checkWinLoseOrDraw(player1: String, player2: String): Int {
     return when {
-        player1 == player2 -> Result.Draw
-        player1 == "rock" && player2 == "scissors" -> Result.Win
-        player1 == "rock" && player2 == "paper" -> Result.Lose
-        player1 == "paper" && player2 == "rock" -> Result.Win
-        player1 == "paper" && player2 == "scissors" -> Result.Lose
-        player1 == "scissors" && player2 == "paper" -> Result.Win
-        player1 == "scissors" && player2 == "rock" -> Result.Lose
-        else -> Result.Draw
+        player1 == player2 -> 3
+        player1 == "rock" && player2 == "scissors" -> 6
+        player1 == "rock" && player2 == "paper" -> 0
+        player1 == "paper" && player2 == "rock" -> 6
+        player1 == "paper" && player2 == "scissors" -> 0
+        player1 == "scissors" && player2 == "paper" -> 6
+        player1 == "scissors" && player2 == "rock" -> 0
+        else -> 3
     }
 }
 
-// write the Result class
-sealed class Result {
-    object Win : Result()
-    object Lose : Result()
-    object Draw : Result()
-}
-
-// create a function which takes a String containing A or B or C and returns rock paper or scissors
-fun convertToRockPaperScissors(input: String): String {
-    return when (input) {
-        "A" -> "rock"
-        "B" -> "paper"
-        "C" -> "scissors"
-        "X" -> "rock"
-        "Y" -> "paper"
-        "Z" -> "scissors"
-        else -> throw IllegalArgumentException("Invalid input")
-    }
-}
-
-// create a function which takes a string containing rock paper or scissors and returns what is needed to win
 fun convertToWinningMove(input: String): String {
     return when (input) {
         "rock" -> "paper"
